2db63a2d7536d0ccc9886e291262996007c632e4
norshine <dconangla@gmail.com>
2014-05-28 01:37:50 +0200

    Preparacio escena
    Tirolina mesh i mats
    Teleport al terrat i navmesh
    Tirolina i cotxes colliders

--- merged/assets/scripts/goUpstairs.cs
+++ merged/assets/scripts/goUpstairs.cs
@@ -4,14 +4,17 @@ using System.Collections;
 public class goUpstairs : UseAction {
 
 	public GameObject mainChar;
+	public ParticleSystem teleportParticles;
 
 	public override void Do () {
-		Debug.Log("Go Upstairs !");
-		NavMeshAgent NA = mainChar.GetComponent("NavMeshAgent") as NavMeshAgent;
+		teleportParticles.Play();
+		Invoke("teleportAmunt", 0.75f);
+	}
 
-
+	private void teleportAmunt() {
+		NavMeshAgent NA = mainChar.GetComponent ("NavMeshAgent") as NavMeshAgent;
 		NA.enabled = false;
-		mainChar.transform.position = new Vector3(-7.440165f, 16.49909f, -11.75213f);
+		mainChar.transform.position = new Vector3 (-7.440165f, 16.49909f, -11.75213f);
 		NA.enabled = true;
 	}
 }
