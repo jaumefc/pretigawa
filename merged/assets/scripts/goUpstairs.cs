1988cc67598ef4c1e8e821f8e5d93f3a7f20b16c
norshine <dconangla@gmail.com>
2014-05-28 00:28:16 +0200

    Teleport al terrat a les escales

---
+++ merged/assets/scripts/goUpstairs.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goUpstairs : UseAction {
+
+	public GameObject mainChar;
+
+	public override void Do () {
+		Debug.Log("Go Upstairs !");
+		NavMeshAgent NA = mainChar.GetComponent("NavMeshAgent") as NavMeshAgent;
+
+
+		NA.enabled = false;
+		mainChar.transform.position = new Vector3(-7.440165f, 16.49909f, -11.75213f);
+		NA.enabled = true;
+	}
+}
