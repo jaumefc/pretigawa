c719463ca5379429dbf8cc5fe89d9eafb7a12510
norshine <dconangla@gmail.com>
2014-08-31 18:51:11 +0200

    Nen i Pitonissa

---
+++ merged/assets/scripts/swingColumpio.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class swingColumpio : MonoBehaviour {
+
+	private int swingForward = -1;
+
+
+	void Start () {
+		Invoke ("changeSwing", 0.8f);
+	}
+	
+	void Update () {
+		rigidbody.angularVelocity = new Vector3 (1 * swingForward, 0, 0);
+	}
+
+	void changeSwing(){
+		swingForward *= -1;
+		Invoke ("changeSwing", 0.8f);
+	}
+}
