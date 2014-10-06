9210a46e635cfd96e18e348d7d299fd44e3401eb
norshine <dconangla@gmail.com>
2014-10-06 13:22:14 +0200

    AudioSex

---
+++ merged/assets/scripts/audioSexController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class audioSexController : MonoBehaviour {
+
+	public AudioSource audio1;
+	public AudioSource audio2;
+	public AudioSource audio3;
+
+	private int audioNum = 0;
+
+	void Start () {
+		playAudioToca ();
+	}
+
+	void Update () {
+	}
+
+	private void playAudioToca(){
+		audioNum++;
+		if (audioNum > 3) audioNum = 1;
+
+		switch (audioNum) {
+			case 1: audio1.Play();
+			break;
+			case 2: audio2.Play();
+			break;
+			case 3:
+			audio3.Play();
+			break;
+		}
+
+		Invoke ("playAudioToca", 10.0f);
+	}
+
+}
