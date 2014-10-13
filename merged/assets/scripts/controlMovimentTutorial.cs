d27fb2e20a73694c692ec69095d21e682ac0f1db
norshine <dconangla@gmail.com>
2014-10-13 12:19:55 +0200

    Preparant escena final

---
+++ merged/assets/scripts/controlMovimentTutorial.cs
@@ -0,0 +1,203 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controlMovimentTutorial : MonoBehaviour {
+	
+	public float speed;
+
+	private GameObject player;
+	private Transform target;
+	private Vector3 startingPosition;
+	private Quaternion startingRotation;
+	private NavMeshAgent navi;
+
+	private lifeController playerLife;
+	public bool hasTarget = false;
+	private Animation animations;
+	private bool imAttacking = false;
+	private bool checkingAttack = false;
+
+	private bool boringIdle = false;
+	private float lastTimeIdle = 0.0f;
+
+	private Vector3 thisNextstep;
+
+	enum enemyState {
+		idle = 0,
+		move = 1,
+		getBack = 2,
+		attack = 3,
+		die = 4,
+	}
+
+	private enemyState _enemyState;
+
+	void Start(){
+		setState (0);
+		startingPosition = transform.position;
+		startingRotation = transform.rotation;
+		navi = GetComponent<NavMeshAgent>();
+		animations = GetComponent<Animation>();
+		player = GameObject.Find("Player");
+		playerLife = player.GetComponent<lifeController> ();
+	}
+
+	public void setState (int state){
+		switch (state) {
+		case 0: _enemyState = enemyState.idle;break;
+		case 1: _enemyState = enemyState.move;break;
+		case 2: _enemyState = enemyState.getBack;break;
+		case 3: _enemyState = enemyState.attack;break;
+		case 4: _enemyState = enemyState.die;break;
+		default: _enemyState = enemyState.idle;break;
+		}
+	}
+
+	public void setTarget(GameObject newTarget){
+		target = newTarget.transform;
+		hasTarget = true;
+	}
+
+	private void selectRandomDestination(){
+		GameObject[] poses = GameObject.FindGameObjectsWithTag ("PlacaPos");
+		int rndPos = Random.Range (0, 9);
+		setTarget(poses [rndPos]);
+		_enemyState = enemyState.move;
+		moute ();
+		lastTimeIdle = 0.0f;
+	}
+
+	void Update () {
+		switch (_enemyState) {
+			case enemyState.idle:
+				if(lastTimeIdle == 0.0f)
+					lastTimeIdle = Time.realtimeSinceStartup;
+
+			break;
+
+			case enemyState.move:
+				if(!imAttacking)
+					moute();
+			break;
+
+			case enemyState.getBack:
+				if(!imAttacking)
+					torna ();
+			break;
+
+			case enemyState.attack:
+				if(GetComponent<controlVisioTutorial>().getIA()){
+					if(!checkingAttack && playerLife.life>0){
+						checkingAttack = true;
+						StartCoroutine(checkAttack(animations.animation["Attack"].length-animations.animation["Attack"].length/4));
+						StartCoroutine(animationEnded(animations.animation["Attack"].length));
+					}else if(playerLife.life <= 0){
+						setState(2);
+					}else
+						animations.CrossFade("Attack");
+				}else{
+					_enemyState = enemyState.idle;
+				}
+			break;
+
+			case enemyState.die:
+			break;
+
+			default:
+				_enemyState = enemyState.idle;
+			break;
+		}
+
+	}
+
+	private void moute(){
+		if (!hasTarget) return;
+
+		Vector3 movement = Vector3.zero;
+
+		navi.SetDestination(target.position);
+		Vector3 nextstep = navi.nextPosition;
+		movement = nextstep - transform.position;
+		movement.y = 0;
+
+		if( navi.velocity.sqrMagnitude < 0.5)
+		{
+			Vector3 distance = target.position - transform.position;
+			float absDistance = distance.sqrMagnitude;
+			if(absDistance <= 2.5 && (target.transform.position == player.transform.position)){
+				imAttacking = true;
+				_enemyState = enemyState.attack;
+			}else{
+				_enemyState = enemyState.idle;
+			}
+		}
+		else
+		{
+			movement = movement.normalized * speed;
+			_enemyState = enemyState.move;
+		}
+	}
+
+	public GameObject getTarget(){
+		return target.gameObject;
+	}
+
+	private void torna(){
+		Vector3 movement = Vector3.zero;
+
+		navi.SetDestination(startingPosition);
+		Vector3 nextstep = navi.nextPosition;
+
+		thisNextstep = nextstep;
+
+		Vector3 distance = startingPosition - transform.position;
+		float absDistance = distance.sqrMagnitude;
+
+		if( absDistance <= 1.5 )
+		{
+			_enemyState = enemyState.idle;
+			transform.rotation = startingRotation;
+		}
+		else
+		{
+			movement = movement.normalized * speed;
+			_enemyState = enemyState.move;
+		}
+	}
+
+	public bool imAtHome(){
+		Vector3 distance = startingPosition - transform.position;
+		float absDistance = distance.sqrMagnitude;
+		if (absDistance <= 1.5)
+						return true;
+
+		return false;
+	}
+
+	public bool checkingForVision(){
+		if (_enemyState != enemyState.attack)
+						return true;
+		return false;
+	}
+
+	IEnumerator checkAttack(float time)
+	{
+		yield return new WaitForSeconds(time);
+		
+		Vector3 distance = target.position - transform.position;
+		float absDistance = distance.sqrMagnitude;
+		if (absDistance < 3) {
+			player.GetComponent<lifeController>().takeOutLife(2.5f);
+		}
+		else {
+			_enemyState = enemyState.move;
+			imAttacking = false;
+		}
+	}
+
+	IEnumerator animationEnded(float time)
+	{
+		yield return new WaitForSeconds(time);
+		checkingAttack = false;
+	}
+}
---
+++ merged/assets/scripts/controlVisioTutorial.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controlVisioTutorial : MonoBehaviour {
+
+	private bool ignoreIA = false;
+
+	private GameObject Player;
+	private RaycastHit hit;
+	private controlMovimentTutorial cm;
+	private bool firstTimeSeen = true;
+	private Animation animations;
+	private float lastTimeSeen = 0.0f;
+
+	void Start () {
+		Player = GameObject.Find ("Player");
+		cm = gameObject.GetComponent<controlMovimentTutorial> ();
+		animations = GetComponent<Animation>();
+	}
+
+	public void changeIAStatus(){
+		ignoreIA = !ignoreIA;
+	}
+
+	public void iaOff(){
+		ignoreIA = true;
+	}
+
+	public void iaOn(){
+		ignoreIA = false;
+	}
+
+	public bool getIA(){
+		return !ignoreIA;
+	}
+
+	void Update () {
+
+		if (cm.checkingForVision () == true) {
+
+			Vector3 compareVector = Player.transform.position - transform.position;
+			float angle = Vector3.Angle (compareVector, transform.forward);
+			float distance = compareVector.sqrMagnitude;
+
+			if (distance < 100 && angle < 60 && !ignoreIA) {
+					Vector3 eyePosition = new Vector3 (transform.position.x, transform.position.y + 1.0f, transform.position.z);
+					Vector3 PlayerEyePosition = new Vector3 (Player.transform.position.x, Player.transform.position.y + 1.3f, Player.transform.position.z);
+
+					Vector3 newVector = PlayerEyePosition - eyePosition;
+					Ray visionRay = new Ray (eyePosition, newVector);
+
+					if (Physics.Raycast (visionRay, out hit)) {
+							if (hit.collider.gameObject == Player) {
+									if(firstTimeSeen){
+										cm.setTarget(gameObject);
+										animations.CrossFade("Angry");
+										Invoke("sawHim", 3);
+										return;
+									}else{
+										if((Time.realtimeSinceStartup - lastTimeSeen) > 10.0f && cm.getTarget() != Player){
+											AudioSource metalAlert = GetComponent<AudioSource>();
+											metalAlert.Play();
+											lastTimeSeen = Time.realtimeSinceStartup;
+										}
+										cm.setTarget (Player);
+										cm.setState (1);
+										return;
+									}
+							}
+							cm.setState (2);
+					}
+			} else if(cm.imAtHome() == false){
+				if(cm.hasTarget == false)
+					cm.setState (2);
+				return;
+			}else{
+				firstTimeSeen = true;
+				cm.setState (0);
+			}
+		}
+	}
+
+	private void sawHim(){
+		firstTimeSeen = false;
+	}
+}
