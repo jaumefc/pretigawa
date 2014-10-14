2421b296196d18279a5174c7596510b0174ceca7
norshine <dconangla@gmail.com>
2014-10-15 00:22:32 +0200

    cursor

---
+++ merged/assets/cursorController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cursorController : MonoBehaviour {
+
+	public Texture2D cursorTexture;
+	public CursorMode cursorMode = CursorMode.Auto;
+	public Vector2 hotSpot = Vector2.zero;
+
+	void Start(){
+		Invoke ("changeCursor", 0.1f);
+	}
+
+	private void changeCursor () {
+		Cursor.SetCursor (cursorTexture, hotSpot, cursorMode);
+	}
+	
+}
