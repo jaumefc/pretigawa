111d23501593ed2f562c07659c42816ff0f4d730
Jaume Fàbrega Clavé <jaumefabregacl@gmail.com>
2014-12-13 16:45:15 +0100

    no message

---
+++ merged/assets/scripts/aliensNotAllowedScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class aliensNotAllowedScript : MonoBehaviour {
+
+	GameObject cartell;
+
+	// Use this for initialization
+	void Start () {
+		cartell = GameObject.Find ("noAliensAllowedCartell");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}	
+
+
+
+	public void disableNoAlliensAllowedCartell(){
+		Invoke ("disable", 2f);
+	}
+
+
+	void disable(){
+		cartell.SetActive (false);
+	}
+}
